5188205ff4cdfdd1069bcfa00a68dc59bf2da86a
EmperorPopo <60585465+EmperorPopo@users.noreply.github.com>
2022-07-30 13:43:14 -0600

    Everything
    Yes

---
+++ src/main/java/frc/robot/commands/DefaultTurretCommand.java
@@ -0,0 +1,16 @@
+package frc.robot.commands;
+import static frc.robot.subsystems.DrivetrainSubsystem.m_navx;
+
+import frc.robot.subsystems.TurretSubsystem;
+
+
+public class DefaultTurretCommand {
+    public void execute(){
+        if (m_navx.getYaw() != 0){
+        
+        }
+        else{
+        
+        }
+    };
+}
